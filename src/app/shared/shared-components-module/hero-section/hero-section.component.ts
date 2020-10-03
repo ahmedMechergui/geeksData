51473ef8fe1c161a319b027a5ec34b37c5dd2c2c
ahmed michrgui <ahmed.michrgui@hotmail.fr>
2020-10-03 19:56:52 +0100

    test

--- src/app/shared/shared-components-module/hero-section/hero-section.component.ts
+++ src/app/shared/shared-components-module/hero-section/hero-section.component.ts
@@ -6,15 +6,10 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./hero-section.component.css']
 })
 export class HeroSectionComponent implements OnInit {
-@Input() imageName;
-@Input() image1 ;
-@Input() image2 = null;
+@Input() isOnHome;
   constructor() { }
 
   ngOnInit(): void {
-    document.getElementById('hero').style.setProperty('background',
-      'linear-gradient(to right, rgba(39, 70, 133, 0.8) 0%, rgba(61, 179, 197, 0.8) 100%), url("assets/images/'
-      + this.imageName + '")');
   }
 
 }
